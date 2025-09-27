0a1ddea8fa7267aa1828316ce0ac50a61d852717
LucaBixx <Luca.bixx93@gmail.com>
2025-09-27 14:49:28 +0200

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
@@ -10,21 +11,24 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _scaleAnim;
+  late AnimationController _rotationController;
 
   @override
   void initState() {
     super.initState();
+
     _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    _scaleAnim =
-        CurvedAnimation(parent: _controller, curve: Curves.easeInOutBack);
+        AnimationController(vsync: this, duration: const Duration(seconds: 2))
+          ..forward();
 
-    _controller.forward();
+    _rotationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..repeat();
 
-    Timer(const Duration(seconds: 4), () {
+    Timer(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (_) => const LoginScreen()),
@@ -35,6 +39,7 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   void dispose() {
     _controller.dispose();
+    _rotationController.dispose();
     super.dispose();
   }
 
@@ -42,13 +47,10 @@ class _SplashScreenState extends State<SplashScreen>
     return AnimatedBuilder(
       animation: _controller,
       builder: (_, __) {
-        final double t = _controller.value;
+        final double t = Curves.easeOutBack.transform(_controller.value);
         return Transform.translate(
           offset: Offset(dx * (1 - t), dy * (1 - t)),
-          child: Opacity(
-            opacity: t,
-            child: Icon(icon, color: color, size: 36 + 12 * t),
-          ),
+          child: Opacity(opacity: t, child: Icon(icon, color: color, size: 38)),
         );
       },
     );
@@ -57,42 +59,49 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.black, Colors.blueGrey],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+      body: AnimatedBuilder(
+        animation: _rotationController,
+        builder: (context, child) {
+          return Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.black, Colors.blueGrey],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
               ),
-            ),
-          ),
-          ScaleTransition(
-            scale: _scaleAnim,
-            child: Image.asset(
-              'assets/icon.png',
-              width: 140,
-              height: 140,
-            ),
-          ),
-          _floatingIcon(Icons.mic, Colors.redAccent, -80, -120),
-          _floatingIcon(Icons.edit, Colors.orangeAccent, 100, -100),
-          _floatingIcon(Icons.camera_alt, Colors.lightBlueAccent, -100, 100),
-          _floatingIcon(Icons.videocam, Colors.greenAccent, 100, 120),
-          const Positioned(
-            bottom: 60,
-            child: Text(
-              "ColorSlash - Versione di Test BETA1",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white70,
-                fontWeight: FontWeight.w500,
+              Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..rotateY(2 * pi * _rotationController.value),
+                child: Image.asset(
+                  'assets/icon.png',
+                  width: 150,
+                  height: 150,
+                ),
               ),
-            ),
-          ),
-        ],
+              _floatingIcon(Icons.mic, Colors.redAccent, -90, -110),
+              _floatingIcon(Icons.edit, Colors.orangeAccent, 100, -100),
+              _floatingIcon(Icons.camera_alt, Colors.cyanAccent, -120, 100),
+              _floatingIcon(Icons.videocam, Colors.greenAccent, 100, 120),
+              const Positioned(
+                bottom: 60,
+                child: Text(
+                  "ColorSlash - Versione di Test BETA1",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
