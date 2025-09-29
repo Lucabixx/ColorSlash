e5890413e8ab82b9f6d052411260248baacb027b
LucaBixx <Luca.bixx93@gmail.com>
2025-09-29 23:48:29 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,23 @@
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'theme_manager.dart';
+import 'theme.dart';
 import 'home_screen.dart';
-import 'settings_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
-  runApp(ThemeManager(prefs: prefs, child: MyApp()));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final themeManager = ThemeManager.of(context);
-    return AnimatedBuilder(
-      animation: themeManager,
-      builder: (context, _) {
-        return MaterialApp(
-          title: 'ColorSlash',
-          theme: themeManager.lightTheme,
-          darkTheme: themeManager.darkTheme,
-          themeMode: themeManager.currentThemeMode,
-          home: HomeScreen(),
-          routes: {
-            '/settings': (_) => SettingsPage(),
-          },
-        );
-      },
+    return MaterialApp(
+      title: 'App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system, // supports both light & dark
+      home: const HomeScreen(),
     );
   }
 }
